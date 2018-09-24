614f98ab80e47f915761c1c15edbca11b22f0dd1
Rafael Makaha Gomes Ferreira <rafael.makaha@gmail.com>
2018-09-24 20:33:54 -0300

    Added time comparison graph plot.
    Signed-off-by: Rafael Makaha Gomes Ferreira <rafael.makaha@gmail.com>

--- tempo.py
+++ tempo.py
@@ -1,4 +1,7 @@
 import matplotlib.pyplot as plt
+import numpy as np
+
+vetor = [6, 8, 0, 7, 9, 1, 3, 4, 2, 5]
 
 avlTime = []
 with open("./avl_time", "r") as f:
@@ -14,5 +17,12 @@ with open("./rb_time", "r") as f:
         rbTime.append(line[:-1])
 f.close
 
-print(avlTime)
-print(rbTime)
+width = 0.3
+x = np.arange(len(vetor))
+
+plt.bar(x, avlTime, width, color="blue", align="center")
+plt.bar(x-width, rbTime, width, color="red", align="center")
+
+plt.xticks(x-width/2, vetor)
+
+plt.show()
